55ead9406d4618d763bb8e8c0f495c68d63c3a65
comtegan <nolan.comte-gaz@etu.univ-smb.fr>
2017-09-26 12:41:22 +0200

    Revert "Test"
    This reverts commit d9f71f55c7cdbd224e3b5a5a715743daa30c1d9c.

--- Scrum/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = new PDO("pgsql:host=localhost port=5433;dbname=duvety","duvety","wHgGzY");
-$result=$db->exec('SET search_path to duvetyscrum');
-
-function db() { global $db; return $db; }
-
-
-
--- Scrum/view/research/index.php
+++
@@ -1,1 +0,0 @@
-REcherche
